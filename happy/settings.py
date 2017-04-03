ed9cbc541fc428584ed5ae53deb2da0179e102ef
felixo <alexei.eremin@gmail.com>
2017-04-03 16:40:18 +0300

    initial 2

--- happy/settings.py
+++ happy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pz&-=-nndtr8nxca*_a-x(*ht)_d+s^y!nk_&#d)vpdewxru$l'
+SECRET_KEY = key.key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'happy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/ae/code/happy/db.conf',
+            'init_command': 'SET default_storage_engine=INNODB',
+        },
     }
 }
 
---
+++ key.py
@@ -0,0 +1,1 @@
+key = 'pz&-=-nndtr8nxca*_a-x(*ht)_d+s^y!nk_&#d)vpdewxru$l'
