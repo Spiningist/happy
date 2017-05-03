c7339203ea61b78abadc1574ab811d5abe4c181b
felixo <alexei.eremin@gmail.com>
2017-05-03 19:39:45 +0300

    working on first row

--- webhappy/models.py
+++ webhappy/models.py
@@ -1,5 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class MainPage(models.Model):
+    name_of_page = models.CharField(max_length=50, default='Главная страница и общая информация')
+    phone_country = models.CharField(max_length=3, default='+7')
+    phone_code = models.CharField(max_length=5, default='925')
+    phone_number = models.CharField(max_length=10, default='346-07-27')
+    email = models.CharField(max_length=30, default='fond-schastie@hotmail.com')
+    youtube = models.CharField(max_length=100, default='https://www.youtube.com/channel/UC20_z5bq_QyONSbVNtb3EVg')
+    fb = models.CharField(max_length=100, default='https://www.facebook.com/fond.schastie/')
+    insta = models.CharField(max_length=100, default='https://www.instagram.com/fond_schastie/')
+    words = models.CharField(max_length=1000, default=' ')
+    title = models.CharField(max_length=100, default=' ')
+    name = models.CharField(max_length=100, default='Диана Менинбаева')
+    photo = models.FileField(upload_to='CEO', default="")
+
+    def __unicode__(self):  # __unicode__ on Python 2
+        return unicode(self.name_of_page) or u''
--- webhappy/views.py
+++ webhappy/views.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from .models import MainPage
 
 def index(request):
-    return render(request, 'webhappy/index.html',{})
+    index_page = MainPage.objects.all()
+    return render(request, 'webhappy/index.html',{ 'index_page': index_page[0]})
