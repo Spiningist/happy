51f9bbae20ffb55a473ab418afd26ea3606074db
Felix <alexei.eremin@gmail.com>
2017-08-02 18:54:17 +0300

    What WE DO

--- webhappy/static/webhappy/script.js
+++ webhappy/static/webhappy/script.js
@@ -68,6 +68,24 @@ var jssor_1_SlideshowTransitions = [
             });
 
             $('#ps_wrap').css( "font", "15px 'DINPro'" );
+
+            $('.popup-gallery').magnificPopup({
+		        delegate: 'a',
+		        type: 'image',
+		        tLoading: 'Loading image #%curr%...',
+		        mainClass: 'mfp-img-mobile',
+		        gallery: {
+		        	enabled: true,
+			        navigateByImgClick: true,
+			        preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		        },
+		        image: {
+			        tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			        titleSrc: function(item) {
+				        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			        }
+		        }
+	        });
 });
 
 function loadingpartner(data)
